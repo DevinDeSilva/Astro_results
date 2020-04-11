1a4403d3e2d8c8e6f9c920a4d46890c55ee58ce3
DevinDeSilva <57395782+DevinDeSilva@users.noreply.github.com>
2020-04-11 16:31:12 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,3 +1,4 @@
+
 //https://docs.google.com/spreadsheets/d/1u-4dTfJfFqf_PJLer_nN2hkPjmiIDtWenHHvVwnG9Z8/edit?ts=5e917eba#gid=0
 //https://docs.google.com/spreadsheets/d/1Y7N4lq5u0mCZ59RbWM9L2iDr9-jQfO111r0JhCvmw_g/edit?ts=5e917f30#gid=0
 
@@ -11,6 +12,7 @@ $(function(){
       "address" :$email.val()
     }
     $result.html("");
+    $result.html("<p>Please wait data loading</p>")
     const school_catogary = new SteinStore(
         "https://api.steinhq.com/v1/storages/5e9187a8b88d3d04ae0816f3"
       );
@@ -22,6 +24,7 @@ $(function(){
     school_catogary.read("Sheet1", { search: { Email: email["address"] } }).then(data => {
         if(!(data[0] == undefined) && !(data[0]["Marks"]==undefined)){
           console.log(data[0]["Marks"]);
+          $result.html("");
           $result.append('<li>Name:- '+data[0]["Name"]+
                         '<br>Email :- '+email["address"]+
                         '<br>Catogary :- School Category'+
@@ -30,20 +33,24 @@ $(function(){
                         + '</li>' );
 
         }else if (!(data[0] == undefined) && (data[0]["Marks"]==undefined)) {
+          $result.html("");
           $result.append('<p>There is an error please contact us.</p>');
         }else{
           open_catogary.read("Sheet1", { search: { Email: email["address"] } }).then(data0 => {
-              if(!(data0[0]["Marks"]==undefined)){
+              if(!(data0[0] == undefined) && !(data0[0]["Marks"]==undefined)){
+                $result.html("");
                 $result.append('<li>Name:- '+data0[0]["Name"]+
                               '<br>Email :- '+email["address"]+
                               '<br>Catogary :- Open Catogary'+
                               '<br> Results :- ' + data0[0]["Marks"]
                               + '</li>' );
               }else if (!(data0[0] == undefined) && (data0[0]["Marks"]==undefined)) {
+                $result.html("");
                 $result.append('<p>There is an error please contact us.</p>');
 
               }else {
-                $result.append('<p>There is an error please contact us.</p>');
+                $result.html("");
+                $result.append('<p>You have entered a invalid email address<br>please try again.</p>');
               }
             });
       }
